c8deb9043c3c059035961e48adbf3962c71eade9
Mouez-01 <moezhajria@gmail.com>
2021-03-18 12:05:34 +0000

    test PC

--- index.php
+++ index.php
@@ -179,7 +179,7 @@ ul {
             <tr>
                 <th>IP</th>
                 <th>Nom d'hote</th>
-                <th>Adresse Mac</th>
+                <th>Adresse Mac / PC 1 </th>
             </tr>
                 </thead>
                 <tbody id="clients">
